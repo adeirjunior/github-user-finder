7136f1e03e4bac8073b324bd7692547ec8eb9453
Adeir do Bom Fim Junior <4adeirj@gmail.com>
2022-07-08 00:13:35 -0400

    rounded image and button color edit

--- islands/Form.tsx
+++ islands/Form.tsx
@@ -8,7 +8,7 @@ export default function Form() {
     return (
         <form method="GET">
             <input type="name" name="q" class={tw`border-2 border-slate-900 px-2 mr-4 rounded-md`} />
-            <button type="submit" class={tw`bg(purple-800 hover:purple-700 focus:purple-700) rounded-md px-4 text-white`}>Find</button>
+            <button type="submit" class={tw`bg(black hover:gray-700 focus:gray-700) rounded-md px-4 text-white`}>Find</button>
         </form>
     )
 }
--- islands/User.tsx
+++ islands/User.tsx
@@ -9,8 +9,8 @@ export default function User({ data, H1 }: UserProp) {
     const H1Clone = () => h(H1.type, { class : H1.props.class }, H1.props.children)
     return (
         <div class={tw`flex flex-col items-center justify-center my-10`}>
-            <div>
-            <img class={tw`mb-4`} src={data.avatar_url} width={256} height={256} />
+            <div class={tw`rounded-full mb-4 overflow-hidden border(black 2)`}>
+                <img src={data.avatar_url} width={256} height={256} />
             </div>
             <H1Clone />
             <p class={tw`text(xl)`}>{data.login}</p>
